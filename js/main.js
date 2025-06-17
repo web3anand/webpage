23c11594b616d13550a89ea0c03da97743889bb8
Hashvalue <88900363+web3anand@users.noreply.github.com>
2025-06-17 17:37:17 +0530

    change in carousel speed

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     let frameId;
-    const speed = 0.5; // pixels per frame
+    const speed = 1.5; // pixels per frame
     const maxScroll = carouselTrack.scrollWidth / 2;
     const startContinuousScroll = () => {
       const step = () => {
