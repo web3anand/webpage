b860378276fc2ed4575414c0ed2a15cd3531a7db
Hashvalue <88900363+web3anand@users.noreply.github.com>
2025-07-15 19:00:53 +0530

    Refactor assets structure and enhance SEO

---
+++ public/assets/js/data.js
@@ -0,0 +1,100 @@
+const projectImages = [
+  { src: "assets/images/a1.jpg", alt: "False ceiling design" },
+  { src: "assets/images/a2.jpg", alt: "Modern flooring work" },
+  { src: "assets/images/a3.jpg", alt: "Roller screen fitting" },
+  { src: "assets/images/a4.jpg", alt: "Aluminium partition office" },
+  { src: "assets/images/a5.jpg", alt: "Aluminium window fitting" },
+  { src: "assets/images/a6.jpg", alt: "False ceiling design" },
+  { src: "assets/images/a7.jpg", alt: "Modern flooring work" },
+  { src: "assets/images/a8.jpg", alt: "Roller screen fitting" },
+  { src: "assets/images/a9.jpg", alt: "Aluminium partition office" },
+  { src: "assets/images/a10.jpg", alt: "Aluminium window fitting" },
+  { src: "assets/images/a11.jpg", alt: "Roller screen fitting" },
+  { src: "assets/images/a12.jpg", alt: "Aluminium partition office" },
+  { src: "assets/images/a13.jpg", alt: "Aluminium window fitting" }
+];
+
+const serviceList = [
+    {
+    name: "Cupboards",
+    services: [
+      { name: "Modular Kitchen", id: "modular", img: "assets/images/modular.jpg" },
+      { name: "TV Showcase", id: "TV", img: "assets/images/tv.jpg" },
+      { name: "UPVC Cupboard", id: "UPVC", img: "assets/images/upvccupboard.jpg" },
+      { name: "Aluminium Cupboard", id: "aluCup", img: "assets/images/aluminiumcupboard.jpg" },
+      { name: "PVC Cupboard", id: "PVC", img: "assets/images/pvccupboard.jpg" },
+      { name: "plywood Cupboard", id: "PlyCup", img: "assets/images/plywood.jpg" },
+      { name: "MDF Cupboard", id: "MDF", img: "assets/images/mdf.jpg" },
+    ]
+  },
+    {
+    name: "Glass Works",
+    services: [
+      { name: "Aluminium Balcony railing", id: "abr", img: "assets/images/a6.jpg" },
+      { name: "12MM Partitions", id: "12mm", img: "assets/images/12mm.jpg" },
+      { name: "Toughen Glass Partitions", id: "toughen", img: "assets/images/toughen.jpg" },
+      { name: "Stainless Steel Balcony railing", id: "ssbr", img: "assets/images/ss.jpg" },
+      { name: "Stairs Glass railing", id: "ssr", img: "assets/images/ssg.jpg" }
+    ]
+  },
+    {
+    name: "Blinds",
+    services: [
+      { name: "Roller Blinds", id: "roller", img: "assets/images/roller.jpg" },
+      { name: "Zebra Blinds", id: "zebra", img: "assets/images/zebra.jpg" },
+      { name: "Roman Blinds", id: "roman", img: "assets/images/roman.jpg" },
+      { name: "PVC Blinds", id: "pvcb", img: "assets/images/pvcblinds.jpg" },
+      { name: "Wooden Blinds", id: "woodb", img: "assets/images/wooden.jpg" },
+      { name: "Curtains", id: "curtain", img: "assets/images/curtains.jpg" },
+      { name: "Venetian Blinds", id: "vb", img: "assets/images/venetian.jpg" },
+      { name: "Bamboo Blinds", id: "bambo", img: "assets/images/bamboo.jpg" }
+    ]
+  },
+  {
+    name: "False Ceiling",
+    services: [
+      { name: "Gypsum Ceiling", id: "gypsum", img: "assets/images/gypsum.jpg" },
+      { name: "Grid Ceiling", id: "grid", img: "assets/images/grid.jpg" },
+      { name: "PVC Ceiling", id: "pvc", img: "assets/images/pvcceiling.jpg" },
+      { name: "Thermocol Ceiling", id: "Thermo", img: "assets/images/thermocol.jpg" },
+    ]
+  },
+  {
+    name: "MosquitoNet",
+    services:[
+      { name: "Open window ", id: "open", img: "assets/images/mosopen.jpg" },
+      { name: "Aluminium Sliding PLeated", id: "aluminium", img: "assets/images/apleated.jpg" },
+      { name: "Honey Comb", id: "sliding", img: "assets/images/honey.jpg" },
+      { name: "Magnetic Door", id: "magnet", img: "assets/images/magnet.jpg" },
+      { name: "Fiber sliding Pleated", id: "fiber", img: "assets/images/fiber.jpg" }
+
+    ]
+  },
+  {
+    name: "Flooring",
+    services: [
+      { name: "Vinyl Flooring", id: "vinyl", img: "assets/images/vinyl flooring.jpg" },
+      { name: "Wooden Flooring", id: "wooden", img: "assets/images/woodenflooring.jpg" },
+      { name: "Carpet ", id: "carpet", img: "assets/images/carpet.jpg" }
+    ]
+  },
+  {
+    name: "Aluminium works",
+    services: [
+      { name: "Aluminium Partition", id: "alupart", img: "assets/images/normalp.jpg" },
+      { name: "Glass Partition", id: "glasspart", img: "assets/images/glassp.jpg" },
+      { name: "Powder Coated Aluminium Partition", id: "powder", img: "assets/images/a1.jpg" },
+      { name: "Sliding Windows", id: "SW", img: "assets/images/windows.jpg" },
+      { name: "Open Windows", id: "OW", img: "assets/images/aopen.jpg" }  
+    ]
+  },
+  {
+    name: "UPVC Work",
+    services: [
+      { name: "UPVC Open window", id: "UPVCopen", img: "assets/images/upvcopen.jpg" },
+      { name: "UPVC sliding window", id: "UPVCS", img: "assets/images/upvcsliding.jpg" },
+      { name: "Fixed", id: "fixed", img: "assets/images/upvcfixed.jpg" },
+      { name: "UPVC Partition", id: "UPVCpart", img: "assets/images/upvcp.jpg" }
+    ]
+  }
+];
